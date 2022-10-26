e95d85ffaa006e26f377d9d78c574fec89cd5679
remi6901 <descollonges.remi@gmail.com>
2022-10-26 10:01:02 +0200

    Create router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from "@angular/router";
+import { ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SignupComponent,
+    UserComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
